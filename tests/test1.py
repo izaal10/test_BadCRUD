d1eee58b6babc07bfe1aaabcc8d0bd65fb9899ae
izaal10 <99273053+izaal10@users.noreply.github.com>
2024-02-05 10:55:26 +0700

    update test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -2,21 +2,32 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-class LoginTest(unittest.TestCase):
+class LoginTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
 
     def setUp(self):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
-        server = 'http://localhost:4444'
-
-        self.browser = webdriver.Remote(command_executor=server, options=options)
-        self.addCleanup(self.browser.quit)
+        # Ensure that the page is loaded before each test
+        self.driver.get("http://localhost/login.php")
 
     def test_valid_login(self):
         self.login("admin", "admin")
-        self.assertIn("http://localhost:4444/index.php", self.browser.current_url)
+        self.assertIn("http://localhost/index.php", self.driver.current_url)
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+    def login(self, username, password):
+        username_input = self.driver.find_element(By.NAME, "username")
+        password_input = self.driver.find_element(By.NAME, "password")
+        submit_button = self.driver.find_element(By.CSS_SELECTOR, ".btn-primary")
+
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+        submit_button.click()
 
 if __name__ == '__main__':
     unittest.main(verbosity=2, warnings='ignore')
